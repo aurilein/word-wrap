bc27c40b61703a22547ce22b9a3e169232fc2e6d
aurilein <auriane@gmx.at>
2017-02-15 17:33:33 +0100

    Refactoring: made line length a field

--- src/LAOLA1/WordWrapper.php
+++ src/LAOLA1/WordWrapper.php
@@ -3,36 +3,44 @@ namespace LAOLA1;
 
 class WordWrapper
 {
+    private $lineLength;
+
+    public function __construct($lineLength = -1)
+    {
+        $this->lineLength = $lineLength;
+    }
+
     public function wrap($text, $lineLength)
     {
-        $this->validateLineLength($lineLength);
+        $this->lineLength = $lineLength;
+        $this->validateLineLength();
 
         $text = trim($text);
-        if ($this->fitsIntoLine($text, $lineLength)) {
+        if ($this->fitsIntoLine($text)) {
             return $text;
         }
 
-        list($firstLine, $remainingText) = $this->splitFirstLine($text, $lineLength);
-        $followingLines = $this->wrap($remainingText, $lineLength);
+        list($firstLine, $remainingText) = $this->splitFirstLine($text);
+        $followingLines = $this->wrap($remainingText, $this->lineLength);
 
         return $firstLine . "\n" . $followingLines;
     }
 
-    private function validateLineLength($lineLength)
+    private function validateLineLength()
     {
-        if ($lineLength < 0) {
-            throw new \InvalidArgumentException("Invalid line length " . $lineLength);
+        if ($this->lineLength < 0) {
+            throw new \InvalidArgumentException("Invalid line length " . $this->lineLength);
         }
     }
 
-    private function fitsIntoLine($text, $lineLength)
+    private function fitsIntoLine($text)
     {
-        return strlen($text) <= $lineLength;
+        return strlen($text) <= $this->lineLength;
     }
 
-    private function splitFirstLine($text, $lineLength)
+    private function splitFirstLine($text)
     {
-        $firstLine = substr($text, 0, $lineLength);
+        $firstLine = substr($text, 0, $this->lineLength);
         $spacePos = strrpos($firstLine, ' ');
         $containsSpace = $spacePos !== false;
 
@@ -40,7 +48,7 @@ class WordWrapper
             $firstLine = substr($text, 0, $spacePos);
             $remainingText = substr($text, $spacePos + 1);
         } else {
-            $remainingText = substr($text, $lineLength);
+            $remainingText = substr($text, $this->lineLength);
         }
 
         return array($firstLine, $remainingText);
--- test/LAOLA1/WordWrapperTest.php
+++ test/LAOLA1/WordWrapperTest.php
@@ -7,49 +7,47 @@ class WordWrapperTest extends \PHPUnit_Framework_TestCase
     private $wordWrapper;
 
     /**
-     * @before
-     */
-    public function initWordWrapper()
-    {
-        $this->wordWrapper = new WordWrapper();
-    }
-
-    /**
      * @test
      * @expectedException \InvalidArgumentException
      */
     public function shouldThrowExceptionOnNegativeLineLength()
     {
+        $this->wordWrapper = new WordWrapper();
         $this->wordWrapper->wrap("", -1);
     }
 
     /** @test */
     public function shouldReturnEmptyText()
     {
+        $this->wordWrapper = new WordWrapper();
         $this->assertEmpty($this->wordWrapper->wrap("", 5));
     }
 
     /** @test */
     public function shouldSplitWordLongerThanLineLength()
     {
+        $this->wordWrapper = new WordWrapper();
         $this->assertEquals("Kat\nze", $this->wordWrapper->wrap("Katze", 3));
     }
 
     /** @test */
     public function shouldSplitWordLongerThanLineLengthMultipleTimes()
     {
+        $this->wordWrapper = new WordWrapper();
         $this->assertEquals("irg\nend\nein\ntex\nt", $this->wordWrapper->wrap("irgendeintext", 3));
     }
 
     /** @test */
     public function shouldSplitPhraseLongerThanLineLengthAtWhitespace()
     {
+        $this->wordWrapper = new WordWrapper();
         $this->assertEquals("ich\nprogrammiere", $this->wordWrapper->wrap("ich programmiere", 13));
     }
 
     /** @test */
     public function shouldSplitPhraseLongerThanLineLengthAtLastWhitespace()
     {
+        $this->wordWrapper = new WordWrapper();
         $this->assertEquals(
             "ich programmiere\njetzt weiter",
             $this->wordWrapper->wrap("ich programmiere jetzt weiter", 18)
@@ -59,18 +57,21 @@ class WordWrapperTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function shouldNotSplitIfPhraseContainsSpaceAndIsShorterThanLineLength()
     {
+        $this->wordWrapper = new WordWrapper();
         $this->assertEquals("ich ein", $this->wordWrapper->wrap("ich ein", 13));
     }
 
     /** @test */
     public function shouldSplitIfSpaceIsDirectlyBeforeLineLength()
     {
+        $this->wordWrapper = new WordWrapper();
         $this->assertEquals("ich\nbin", $this->wordWrapper->wrap("ich bin", 4));
     }
 
     /** @test */
     public function shouldRemoveSpaceDirectlyAfterLineLength()
     {
+        $this->wordWrapper = new WordWrapper();
         $this->assertEquals("ich\nbin", $this->wordWrapper->wrap("ich bin", 3));
     }
 }
